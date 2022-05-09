03978c597635f69701efe6ff47e1bcc661da063a
Vitaly Efimov <footballervit@mail.ru>
2022-05-09 12:35:54 +0800

    Initial commit

---
+++ src/main/java/ru/topjava/efimov/EfimovApplication.java
@@ -0,0 +1,13 @@
+package ru.topjava.efimov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfimovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EfimovApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/topjava/efimov/EfimovApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.topjava.efimov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfimovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
